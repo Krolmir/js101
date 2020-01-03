0a1438f5abfb2587e990d32a8d52aaf1c466a746
Krolmir <nicholasmendes@hotmail.com>
2020-01-03 17:41:30 +0000

    Updates to rock paper scissors

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -5,6 +5,13 @@ const VALID_CHOICES = { r: 'rock',
                         l: 'lizard',
                         sp: 'spock' };
 
+let userChoice = '';
+let randomIndex = '';
+let computerChoice = '';
+let playerTotal = 0;
+let computerTotal = 0;
+let playAgain = '';
+
 function clearScreen() {
   console.clear();
 }
@@ -17,8 +24,11 @@ function displayChoices(userChoice, computerChoice) {
   prompt(`You chose ${userChoice}, computer chose ${computerChoice}.`);
 }
 
-function displayChooseOne() {
-  prompt(`Choose one`);
+function displayGameWinner() {
+  clearScreen();
+  displaySpacerMedium();
+  displayChoices(userChoice, computerChoice);
+  displayWinnerMessage(userChoice, computerChoice);
 }
 
 function displayGrandWinner(playerWinTotal) {
@@ -32,39 +42,91 @@ function displayGrandWinner(playerWinTotal) {
 }
 
 function displayInvalidChoice() {
+  clearScreen();
   prompt("That's not a valid choice");
+  displayOptions();
 }
 
-function displayInvalidPlayAgain() {
-  prompt('That is an invalid option.');
-  prompt('Please enter "yes" or "no"):\n');
+function displayOptions() {
+  prompt(`Choose one`);
+  for (const PROPERTY in VALID_CHOICES) {
+    prompt(`"${PROPERTY}" for ${VALID_CHOICES[PROPERTY]}.`);
+  }
+  console.log('');
 }
 
-function displayPlayAgain() {
+function getPlayAgain() {
+  displayPlayAgainMessage();
+
+  playAgain = readline.question().toLowerCase();
+
+  while (playAgain !== 'no' && playAgain !== 'yes') {
+    clearScreen();
+    prompt('That is an invalid option.');
+    displayPlayAgainMessage();
+    playAgain = readline.question().toLowerCase();
+  }
+  clearScreen();
+}
+
+function displayPlayAgainMessage() {
   displaySpacerMedium();
   prompt('Would you like to play again? (yes/no)');
   displaySpacerMedium();
 }
 
 function displayScoreBoard(playerWinTotal, computerWinTotal) {
-  console.log('________________________');
-  console.log('|');
-  console.log(`|  Player 1 Score: ${playerWinTotal}`);
-  console.log(`|  Computer Score: ${computerWinTotal}`);
-  console.log('|_______________________\n');
+  console.log(' _____________________');
+  console.log('|                     |');
+  console.log(`|  Player 1 Score: ${playerWinTotal}  |`);
+  console.log(`|  Computer Score: ${computerWinTotal}  |`);
+  console.log('|_____________________|\n');
+}
+
+function displaySpacerLarge() {
+  console.log('--------------------------------------------------------------');
 }
 
 function displaySpacerMedium() {
   console.log('-----------------------------------------');
 }
 
-function displayWinner(userChoice, computerChoice) {
+function displayWelcomeMessage() {
+  clearScreen();
+  displaySpacerLarge();
+  console.log('      Welcome to Rock Paper Scissors Lizard and Spock!');
+  console.log('You will be playing against the computer in a race to 5 wins. ');
+  console.log('                  GOODLUCK!!!!!!');
+  displaySpacerLarge();
+}
+function displayWinnerMessage(userChoice, computerChoice) {
   if (isWinner(userChoice, computerChoice)) {
     prompt('You win!');
   } else if (isWinner(computerChoice, userChoice)) {
     prompt('You lose.');
   } else {
     prompt("It's a tie");
+  }
+}
+
+function getComputerMove() {
+  randomIndex = Math.floor(Math.random() *
+                Object.values(VALID_CHOICES).length);
+  computerChoice = Object.values(VALID_CHOICES)[randomIndex];
+}
+
+function getPlayerMove() {
+  displayOptions();
+  userChoice = readline.question().toLowerCase();
+
+  while (!Object.keys(VALID_CHOICES).includes(userChoice) &&
+        !Object.values(VALID_CHOICES).includes(userChoice)) {
+    displayInvalidChoice();
+    userChoice = readline.question();
+  }
+
+  if (userChoice.length < 3) {
+    userChoice = VALID_CHOICES[userChoice];
   }
 }
 
@@ -92,44 +154,30 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function resetTotals() {
+  playerTotal = 0;
+  computerTotal = 0;
+}
+
+function updateScore() {
+  if (playerWin(userChoice, computerChoice)) {
+    playerTotal += 1;
+  } else if (computerWin(userChoice, computerChoice)) {
+    computerTotal += 1;
+  }
+}
+
 while (true) {
 
-  let playerTotal = 0;
-  let computerTotal = 0;
+  displayWelcomeMessage();
+  resetTotals();
 
   while (true) {
 
-    displayChooseOne();
-    for (const PROPERTY in VALID_CHOICES) {
-      prompt(`"${PROPERTY}" for ${VALID_CHOICES[PROPERTY]}.`);
-    }
-    let userChoice = readline.question().toLowerCase();
-
-    while (!Object.keys(VALID_CHOICES).includes(userChoice) &&
-           !Object.values(VALID_CHOICES).includes(userChoice)) {
-      displayInvalidChoice();
-      userChoice = readline.question();
-    }
-
-    if (userChoice.length < 3) {
-      userChoice = VALID_CHOICES[userChoice];
-    }
-
-    let randomIndex = Math.floor(Math.random() *
-                      Object.values(VALID_CHOICES).length);
-    let computerChoice = Object.values(VALID_CHOICES)[randomIndex];
-
-    clearScreen();
-    displaySpacerMedium();
-    displayChoices(userChoice, computerChoice);
-    displayWinner(userChoice, computerChoice);
-
-    if (playerWin(userChoice, computerChoice)) {
-      playerTotal += 1;
-    } else if (computerWin(userChoice, computerChoice)) {
-      computerTotal += 1;
-    }
-
+    getPlayerMove();
+    getComputerMove();
+    displayGameWinner();
+    updateScore();
     displayScoreBoard(playerTotal, computerTotal);
 
     if (grandWinner(playerTotal) || grandWinner(computerTotal)) {
@@ -138,17 +186,9 @@ while (true) {
   }
 
   displayGrandWinner(playerTotal);
-  displayPlayAgain();
-
-  let playAgain = readline.question().toLowerCase();
-
-  while (playAgain !== 'no' && playAgain !== 'yes') {
-    displayInvalidPlayAgain();
-    playAgain = readline.question().toLowerCase();
-  }
+  getPlayAgain();
 
   if (playAgain === 'no') {
     break;
   }
-  clearScreen();
 }
