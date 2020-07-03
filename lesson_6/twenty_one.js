30b8048ca47c13f5a1139d8ca5372dec1472592e
Krolmir <nicholasmendes@hotmail.com>
2020-07-03 22:25:14 +0000

    twenty one stuff

--- lesson_6/twenty_one.js
+++ lesson_6/twenty_one.js
@@ -19,11 +19,20 @@
 // Create a deck of cards
 
 const readLineSync = require('readline-sync');
-const CARDNUMBERS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const CARDNUMBERS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'A', 'A', 'A'];
 const SUITS = ['♠', '♥', '♦', '♣'];
 let playerCards = [];
 let dealerCards = [];
 let freshDeck = [];
+let playerTotal, dealerTotal;
+
+function prompt(msg = "") {
+  console.log("=> " + msg);
+}
+
+function emptyLine() {
+  console.log(' ');
+}
 
 function displayTableIntro() {
   console.log('____________________________________');
@@ -38,21 +47,51 @@ function displayTableIntro() {
   console.log('|__________________________________|');
 }
 
-function displayTableInitialDeal() {
+function displayTable() {
   console.log('____________________________________');
-  console.log('|                                  |');
-  console.log('|Dealer Total:                     |');
+  console.log('|                                  ');
+  console.log('|Dealer Total:                     ');
   console.log(`|    |${dealerCards[0].join('')}| |${dealerCards[1].join('')}|`);
-  console.log('|                                  |');
-  console.log('|                                  |');
-  console.log('|                                  |');
-  console.log('|Player Total:                     |');
-  console.log(`|    |${playerCards[0].join('')}| |${playerCards[1].join('')}|`);
-  console.log('|                                  |');
-  console.log('|__________________________________|');
+  console.log('|                                  ');
+  console.log('|                                  ');
+  console.log('|                                  ');
+  console.log(`|Player Total: ${playerTotal}  `);
+  console.log(`|    ${displayCardsOnTable(playerCards)}`);
+  console.log('|                                  ');
+  console.log('|__________________________________');
 }
 
+function displayCardsOnTable(cards) {
+  let string = '';
 
+  cards.forEach(card => {
+    string += `|${card.join('')}|`;
+  });
+  return string;
+}
+
+function calculateTotal(cards) {
+  let total = 0;
+  let aceCounter = 0;
+
+  cards.forEach(card => {
+    if (card[0] === 'A') {
+      total += 11;
+      aceCounter += 1;
+    } else if ((card[0] === 'J' ) || (card[0] === 'Q') || (card[0] === 'K' )) {
+      total += 10;
+    } else {
+      total += Number.parseInt(card[0]);
+    }
+  });
+
+  while ((total > 21) && (aceCounter > 0)) {
+    total -= 10;
+    aceCounter -= 1;
+  };
+
+  return total;
+}
 
 function initializeDeck() {
   let deck = [];
@@ -72,8 +111,8 @@ function initialCardsDealt() {
   }
 }
 
-function dealACard(name, deck) {
-  name.push(deck.pop());
+function dealACard(cards, deck) {
+  cards.push(deck.pop());
 }
 
 function shuffleCards(deck) {
@@ -90,12 +129,32 @@ console.clear();
 freshDeck = initializeDeck();
 shuffleCards(freshDeck);
 initialCardsDealt(freshDeck);
-displayTableInitialDeal();
+playerTotal = calculateTotal(playerCards);
+displayTable();
 
+while (true) {
+  displayHitOrStayPrompt();
+  let input = readLineSync.question();
+  while (!isValid(input)) {
+    console.log('That is not a valid option.');
+    prompt("Please enter 'H' or 'h' for hit and 'S' or 's' for stay:");
+  input = readLineSync.question();
+  }
 
-console.log(playerCards);
-console.log(dealerCards);
+  if ((input === 's') || (input === 'S')) break;
 
+  dealACard(playerCards, freshDeck);
+  playerTotal = calculateTotal(playerCards);
+  displayTable();
+}
 
+function isValid(input) {
+  return ((input === 's') || (input === 'S') ||
+          (input === 'h') || (input === 'H'));
+}
 
-
+function displayHitOrStayPrompt() {
+  emptyLine();
+  prompt('Would you like to Hit or Stay?');
+  prompt("Please enter 'h' or 'H' for hit and 's' or 'S' for stay:");
+}
